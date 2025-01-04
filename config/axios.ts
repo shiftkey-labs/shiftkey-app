0eec705ae46ec4eea39eec6902a65b15e58cd0df
Vansh Sood <emailofvansh@gmail.com>
2025-01-03 22:31:40 -0400

    styling, auth, sm fixes

--- config/axios.ts
+++ config/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const PROD_URL = process.env.BACKEND_URL;
-export const DEV_URL = "http://192.168.2.205:3000";
+
 
 const server = axios.create({
-  baseURL: DEV_URL,
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
