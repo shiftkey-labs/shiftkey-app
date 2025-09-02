abb277ce0189d5ac6b1ec1ee577e6f6df7af9479
Vansh983 <emailofvansh@gmail.com>
2025-09-02 15:56:26 -0300

    fix

--- config/axios.ts
+++ config/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const PROD_URL = process.env.BACKEND_URL;
 
 const server = axios.create({
+  baseURL: "https://shiftkey.vanshsood.com",
   headers: {
     "Content-Type": "application/json",
   },
